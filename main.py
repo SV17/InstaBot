fb78f3c2f8373da786308aceecbdf3ceb2116ce6
Shikha Verma <shikha.17.sv@gmail.com>
2018-02-18 03:29:46 +0530

    Main modified

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ BASE_URL='https://api.instagram.com/v1/'
 
 def self_info():
     r = requests.get(("%susers/self/?access_token=%s") % (BASE_URL,APP_ACCESS_TOKEN)).json()
+    print "My Info is :\n"
     if r['meta']['code'] == 200:
         # Code to print user's details
         if 'data' in r:
-            print 'Username: %s' % (r['data']['username'])
-            print 'Full name: %s ' % (r['data']['full_name'])
+            print 'ID : %s' %(r['data']['id'])
+            print 'Username : %s' % (r['data']['username'])
+            print 'Full name : %s ' % (r['data']['full_name'])
+            print 'Profile picture URL : %s' %(r['data']['profile_picture'])
+            print 'Short bio : %s' %(r['data']['bio'])
+            print 'Website : %s' %(r['data']['website'])
+            print 'Business profile(True(Yes) or False(No) : %s' %(r['data']['is_business'])
             print 'Number of followers: %s' % (r['data']['counts']['followed_by'])
-            print 'Number of people you are following: %s' % (r['data']['counts']['follows'])
+            print 'Number of people I am following: %s' % (r['data']['counts']['follows'])
             print 'Number of posts: %s' % (r['data']['counts']['media'])
         else:
             print 'User does not exist!'
@@ -23,13 +29,13 @@ def self_info():
 
 def get_user_id(insta_username):
 
-    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
-    print "Get request url: %s" % (request_url)
-    user_info = requests.get(request_url).json()
-    if user_info['meta']['code'] == 200:
+    r = requests.get((BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)).json()
 
-        if len(user_info['data']):
-            print user_info['data'][0]['id']
+    if r['meta']['code'] == 200:
+
+        if len(r['data']):
+            print "User ID : "
+            print (r['data'][0]['id'])
         else:
             return None
     else:
@@ -45,18 +51,16 @@ def get_user_info(insta_username):
         print 'User does not exist!'
         exit()
 
-    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)
-    user_info = requests.get(request_url).json()
+    r = requests.get((BASE_URL + 'users/{user-id}/?access_token==%s') % (user_id, APP_ACCESS_TOKEN)).json()
 
-    if user_info['meta']['code'] == 200:
+    if r['meta']['code'] == 200:
 
-        if 'data' in user_info:
+        if 'data' in r:
 
-            print 'Username: %s' % (user_info['data']['username'])
-            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
-            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
-            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+            print 'Username: %s' % (r['data']['username'])
+            print 'No. of followers: %s' % (r['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (r['data']['counts']['follows'])
+            print 'No. of posts: %s' % (r['data']['counts']['media'])
 
         else:
 
@@ -70,14 +74,14 @@ def start_bot():
     show_menu = True
     while show_menu:
 
-         print "*******<< Welcome to INSTABOT >>*******"
+         print "\n*******<< Welcome to INSTABOT >>*******"
          menu_choice = input("\nWhat do you want to do? \n 1. Get your own details\n 2. Get user\'s details by the username.\n 3. Get user's info\n 0. Exit\n")
 
          if menu_choice == 1:
              self_info()
          elif menu_choice == 2:
-             username = raw_input("Enter the username of the user: ")
-             get_user_id(username)
+             insta_username = raw_input("Enter the username of the user: ")
+             get_user_id(insta_username)
          elif menu_choice == 3:
              username = raw_input("Enter the username of the user: ")
              get_user_info(username)
