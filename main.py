96c1b614634de474a55b64a784312df417cd696a
Shikha Verma <shikha.17.sv@gmail.com>
2018-02-18 11:38:35 +0530

    User's Info Module Completed

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+# Importing requests
 import requests
-from pprint import pprint
 
-#response = requests.get("https://json.io/b/59d0f30408be13271f7df29c").json()
-APP_ACCESS_TOKEN = '1395856984.a729afe.d9003eb28fcc45c2ac8e2fab6e923ffb'
+# Importing termcolor for color
+from termcolor import colored
+
+# Takes the URL from user and coverts it into json format
+response = requests.get('https://api.jsonbin.io/b/59d0f30408be13271f7df29c').json()
+# Gets the access token in response
+APP_ACCESS_TOKEN = response['access_token']
+# Base URL remains same so storing it in a variable
 BASE_URL='https://api.instagram.com/v1/'
 
+# Method to display self info
 def self_info():
     r = requests.get(("%susers/self/?access_token=%s") % (BASE_URL,APP_ACCESS_TOKEN)).json()
-    print "My Info is :\n"
-    if r['meta']['code'] == 200:
+    print colored("\nMy Info is : ","magenta")
+    if r['meta']['code'] == 200:                # HTTP 200 means transmission is OK
         # Code to print user's details
         if 'data' in r:
             print 'ID : %s' %(r['data']['id'])
@@ -27,69 +34,74 @@ def self_info():
     else:
         print 'Status code other than 200 received!'
 
+# Method to get the user id
 def get_user_id(insta_username):
-
-    r = requests.get((BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)).json()
-
-    if r['meta']['code'] == 200:
-
-        if len(r['data']):
-            print "User ID : "
-            print (r['data'][0]['id'])
+    info = requests.get((BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)).json()
+    
+    if info['meta']['code'] == 200:  # HTTP 200 means transmission is OK
+        if len(info['data']):               # Checking length using len() function
+            return info['data'][0]['id']
         else:
             return None
     else:
-        print 'status code other than 200.'
-        exit()
+        print 'Status code other than 200 received!'      # If transmission is not OK
 
-def get_user_info(insta_username):
 
+# Method to get the information about the user
+def get_info(insta_username):
     user_id = get_user_id(insta_username)
-
     if user_id == None:
-
         print 'User does not exist!'
         exit()
-
-    r = requests.get((BASE_URL + 'users/{user-id}/?access_token==%s') % (user_id, APP_ACCESS_TOKEN)).json()
-
-    if r['meta']['code'] == 200:
-
-        if 'data' in r:
-
-            print 'Username: %s' % (r['data']['username'])
-            print 'No. of followers: %s' % (r['data']['counts']['followed_by'])
-            print 'No. of people you are following: %s' % (r['data']['counts']['follows'])
-            print 'No. of posts: %s' % (r['data']['counts']['media'])
-
-        else:
-
-            print 'There is no data for this user!'
     else:
+        info = requests.get((BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)).json()
+        print colored("\nUser Info is :","magenta")
+        if info['meta']['code'] == 200:
+            if len(info['data']):
+                print 'ID : %s' % (info['data']['id'])
+                print 'Username : %s' % (info['data']['username'])
+                print 'Full name : %s ' % (info['data']['full_name'])
+                print 'Profile picture URL : %s' % (info['data']['profile_picture'])
+                print 'Short bio : %s' % (info['data']['bio'])
+                print 'Website : %s' % (info['data']['website'])
+                print 'Business profile(True(Yes) or False(No) : %s' % (info['data']['is_business'])
+                print 'Number of followers: %s' % (info['data']['counts']['followed_by'])
+                print 'Number of people you are following: %s' % (info['data']['counts']['follows'])
+                print 'Number of posts: %s' % (info['data']['counts']['media'])
+            else:
+                print 'There is no data for this user!'
+        else:
+            print 'Status code other than 200 received!'
 
-        print 'Status code other than 200 received!'
+print colored("\n*******<< Welcome to INSTABOT >>*******","magenta")
 
+
+# Starting application
 
 def start_bot():
+    # Initializing show_menu with True value
     show_menu = True
     while show_menu:
-
-         print "\n*******<< Welcome to INSTABOT >>*******"
-         menu_choice = input("\nWhat do you want to do? \n 1. Get your own details\n 2. Get user\'s details by the username.\n 3. Get user's info\n 0. Exit\n")
+         # Asking the user's choice
+         menu_choice = input(colored("\nWhat do you want to do? \n 1. Get your own details\n 2. Get user\'s id by the username\n 3. Get user's information\n 0. Exit\n","blue"))
 
          if menu_choice == 1:
-             self_info()
+             self_info()           # Calling method
          elif menu_choice == 2:
+             # Asking the user name
              insta_username = raw_input("Enter the username of the user: ")
-             get_user_id(insta_username)
+             user_id = get_user_id(insta_username)          # Calling get_user_id method
+             print(colored("User ID is : %s ","magenta")) % (user_id)
          elif menu_choice == 3:
+             # Asking the user name
              username = raw_input("Enter the username of the user: ")
-             get_user_info(username)
-
-         elif menu_choice == 0:
+             get_info(username)                       # Calling the get_info method
+         elif menu_choice ==0:
              show_menu = False
 
          else:
+            # If user chooses something other than the menu choices
             print "Invalid choice!!!"
 
+# Calling the start_bot() method to start the application
 start_bot()
